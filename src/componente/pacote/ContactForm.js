d9458f19acccdb593bef44a5b11353217b9ea814
RonaldoBelem <ronaldonunes8@gmail.com>
2024-07-24 15:21:33 -0300

    ajuste

--- src/componente/pacote/ContactForm.js
+++ src/componente/pacote/ContactForm.js
@@ -17,6 +17,7 @@ const ContactForm = () => {
 
       <input type="hidden" name="_captcha" value="false" />
       <input type="hidden" name="_next" value="https://ronaldo-belem.vercel.app/Sobre" />
+      <input type="hidden" name="_autoresponse" value="Obrigado Pelo Contato, Em Breve Entrarei Em Contato"></input>
       <input className="btnform" type="submit" value="Submit" />
     </form>
   );
