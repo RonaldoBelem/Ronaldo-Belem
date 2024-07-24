4d56e3b68cc880356bba1693c91bedbaa1929841
RonaldoBelem <ronaldonunes8@gmail.com>
2024-07-24 13:59:28 -0300

    ajuste

--- src/componente/pacote/ContactForm.js
+++ src/componente/pacote/ContactForm.js
@@ -1,77 +1,25 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from "react";
+import "../css/Sobre.css";
 
 const ContactForm = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
+  return (
+    <form
+      
+      action="https://formsubmit.co/ronaldonunes8@gmail.com"
+      method="POST"
+    >
+      <label for="name">Nome</label>
+      <input id="name" type="text" name="name" />
+      <label for="email">Email</label>
+      <input id="email" type="text" name="email" />
+      <label for="message">message</label>
+      <textarea id="message" name="message"></textarea>
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post('http://localhost:3001/send', formData)
-            .then((response) => {
-                console.log(response.data);
-                // Limpar o formulário após o envio
-                setFormData({
-                    name: '',
-                    email: '',
-                    message: ''
-                });
-            })
-            .catch((error) => {
-                console.error('Erro ao enviar o email:', error);
-            });
-    };
-
-    return (
-        <div className="container">
-            <form onSubmit={handleSubmit}>
-                <h2>Formulário de Contato</h2>
-
-                <label htmlFor="name">Nome</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                />
-
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-
-                <label htmlFor="message">Mensagem</label>
-                <textarea
-                    id="message"
-                    name="message"
-                    rows="4"
-                    value={formData.message}
-                    onChange={handleChange}
-                    required
-                ></textarea>
-
-                <button className='btnform' type="submit">Enviar</button>
-            </form>
-        </div>
-    );
+      <input type="hidden" name="_captcha" value="false" />
+      <input type="hidden" name="_next" value="https://ronaldo-belem.vercel.app/Sobre" />
+      <input className="btnform" type="submit" value="Submit" />
+    </form>
+  );
 };
 
 export default ContactForm;
